659e5b49c67195726b4067fe544dd40cc5e25355
Chylong Nou <chylongnou@gmail.com>
2025-08-29 10:10:20 +0700

    re deploy 2

--- scripts/check-env.js
+++ scripts/check-env.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+require('dotenv').config({ path: '.env.local' });
+
 // Simple script to check if required environment variables are set
 const requiredEnvVars = [
   'NEXT_PUBLIC_SUPABASE_URL',
